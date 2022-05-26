eaa432cafe6e15419220990bf8087d2f7919f9f2
alibit3 <105742017+alibit3@users.noreply.github.com>
2022-05-27 01:06:56 +0300

    Update الحساب.py

--- iqthon/plugins/الحساب.py
+++ iqthon/plugins/الحساب.py
@@ -1054,7 +1054,7 @@ async def _(event):
         await catevent.edit(str(e))
     else:
         if photo:
-            await catevent.edit("**♛ ⦙   أشترك @IQTHON **")
+            await catevent.edit("**♛ ⦙   أشترك @RS_SY **")
             if photo.endswith((".mp4", ".MP4")):
                 # https://t.me/tgbetachat/324694
                 size = os.stat(photo).st_size
